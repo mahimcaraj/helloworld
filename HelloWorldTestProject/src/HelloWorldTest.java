065bc4e41f8e2b699e91bebf06e1413d4c496786
Maheshwari Vengala <mahima.here@gmail.com>
2017-09-28 14:33:16 -0400

    Hello World Initial commit

---
+++ HelloWorldTestProject/src/HelloWorldTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Maheshwari Vengala
+ *
+ */
+public class HelloWorldTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+
+	}
+
+}
