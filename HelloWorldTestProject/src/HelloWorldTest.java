1e9145d078c0191dde425dcac9848efcd2647128
Maheshwari Vengala <mahima.here@gmail.com>
2017-09-29 14:48:55 -0400

    GIT Test-1

--- HelloWorldTestProject/src/HelloWorldTest.java
+++ HelloWorldTestProject/src/HelloWorldTest.java
@@ -12,13 +12,8 @@ public class HelloWorldTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello world");
-		System.out.println("How are you doing?");
-		System.out.println("GIT test");
-		System.out.println("GIT test-1");
-		System.out.println("GIT test-3");
-		System.out.println("GIT test-2");
+		// TODO Auto-generated method stub		
+		System.out.println("GIT test-1");		
 	}
 
 }
